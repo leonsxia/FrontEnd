0748ac3fd70de4a55c706cf6771263135df7f212
leonsxia_home <leon_s_xia@126.com>
2024-03-15 01:53:05 +0800

    Fixed framerate, add stage jump and fix bugs

--- learning-area/javascript/canvas/canvas_walking_animation/script.js
+++ learning-area/javascript/canvas/canvas_walking_animation/script.js
@@ -3,6 +3,7 @@
     const height = canvas.height = window.innerHeight;
     const ctx = canvas.getContext('2d');
     const groundLevel = height / 4;
+    let frames = 0;
 
     ctx.fillStyle = 'rgb(0,0,0)';
     ctx.fillRect(0, 0, width, height);
@@ -24,9 +25,9 @@
         obstacles.push(new Obstacle([-width / 2 + 300, 74], [width - 300, height / 4 - 74], obstacleStyle, obstacleTypes[1]));
         obstacles.push(new Obstacle([200, -6], [150, 80], obstacleStyle, obstacleTypes[1]));
         obstacles.push(new Obstacle([275, -56], [150, 100], obstacleStyle, obstacleTypes[1]));
-        obstacles.push(new Obstacle([-400, -200], [150, 10], obstacleStyle, obstacleTypes[1]));
+        obstacles.push(new Obstacle([-400, -250], [150, 10], obstacleStyle, obstacleTypes[1]));
         obstacles.push(new Obstacle([-100, -160], [150, 10], obstacleStyle, obstacleTypes[1]));
-        run();
+        run(0);
         // draw();
     });
 
@@ -37,6 +38,7 @@
             this.spriteWidth = spriteWidth;
             this.spriteHeight = spriteHeight;
             this.notCollisionColor = color;
+            this.spriteFrames = 0;
         }
 
         get collisionCenterX() {
@@ -87,6 +89,7 @@
             this.velX = 2;
             this.velY = 9;
             this.velXFast = 6;
+            this.velG = 0;
             this.direction = 'right';
             this.imageLeft = imageLeft;
             this.imageRight = imageRight;
@@ -94,8 +97,7 @@
             this.jumpMaxHeight = 200;
             this.frameRate = frameRate;
             this.fastFrameRate = fastFrameRate;
-            this.frame = 0;
-            this.jumpTime = 0;
+            // this.jumpTime = 0;
             this.inAirFrame = 0;
             this.elapsedTime = 0;
             this.gravity = 9.8;
@@ -103,6 +105,8 @@
             this.downEffect = 2.6;
             this.elapsedMSPerFrame = 1000 / 60;
             this.color = color;
+            this.jumpTimes = 0;
+            this.topJumpTimes = 1;
         }
 
         get bottomY() {
@@ -153,7 +157,12 @@
                         this.#fasten = true;
                         break;
                     case ' ':
-                        if (this.bottomY === groundLevel) {
+                        if (this.bottomY === groundLevel || (!this.#jump && this.velG > 0 && this.jumpTimes < this.topJumpTimes)) {
+                            // console.log(`${this.bottomY} ${this.jumpTimes} ${this.velG} ${this.jumpTimes < this.topJumpTimes && this.bottomY < groundLevel}`);
+                            if (this.jumpTimes < this.topJumpTimes && this.bottomY < groundLevel && !this.#bottomBlock) {
+                                this.jumpTimes++;
+                            }                            
+                            
                             this.resetJumpStatus(true);
                         }
                         this.#SpaceKeyDown = true;
@@ -194,7 +203,7 @@
                         this.#fasten = false;
                         break;
                     case ' ':
-                        this.#jump = false;
+                        this.resetJumpStatus(false);
                         this.#SpaceKeyDown = false;
                         break;
                 }
@@ -203,7 +212,7 @@
 
         checkMovement() {
             if (this.#isMove) {
-                if (this.frame % (this.#fasten ? this.fastFrameRate : this.frameRate) === 0) {
+                if (this.spriteFrames % (this.#fasten ? this.fastFrameRate : this.frameRate) === 0) {
                     if (this.sprite === 5) {
                         this.sprite = 0;
                     } else {
@@ -215,10 +224,10 @@
                 } else {
                     this.posX -= this.#fasten ? this.velXFast : this.velX;
                 }
-                this.frame++;
+                this.spriteFrames++;
             } else {
                 this.sprite = 0;
-                this.frame = 0;
+                this.spriteFrames = 0;
             }
             
             if (this.#jump) {
@@ -227,15 +236,15 @@
                     this.elapsedTime += this.elapsedMSPerFrame / 1000;
                     // console.log(`${this.elapsedTime}ms`);
                 }
-                let velG = this.velY - this.upEffect * this.gravity * this.elapsedTime
-                if (velG > 0 ) { //&& this.posY > -this.jumpMaxHeight
-                    this.posY -= velG;
+                this.velG = this.velY - this.upEffect * this.gravity * this.elapsedTime
+                if (this.velG > 0 ) { //&& this.posY > -this.jumpMaxHeighta
+                    this.posY -= this.velG;
                     // console.log(this.velY * this.elapsedTime);
                     this.sprite = this.direction === 'right' ? 2 : 5;
                 } else {
                     this.resetJumpStatus(false); // fall at top
                 }
-            } else if (this.bottomY < groundLevel) { // falling
+            } else if (this.bottomY < groundLevel && !this.#bottomBlock) { // falling
                 this.inAirFrame++;
                 if (this.inAirFrame % 1 === 0) {
                     this.elapsedTime += this.elapsedMSPerFrame / 1000;
@@ -243,8 +252,10 @@
                 }
                 this.posY += this.velY + this.downEffect * this.gravity * this.elapsedTime;
                 // console.log(`deltaVelY: ${this.velY + this.downEffect * this.gravity * this.elapsedTime}`);
-                if (this.bottomY >= groundLevel) {
+                if (this.bottomY > groundLevel) {
                     this.posY = groundLevel - this.spriteHeight;
+                    this.jumpTimes = 0;
+                    this.velG = 0;
                     if (this.#SpaceKeyDown) {
                         this.resetJumpStatus(true);
                     }
@@ -265,7 +276,7 @@
                 if (dy < attachHeight) {
                     let xgap = this.#fasten ? this.velXFast : this.velX;
                     
-                    if (this.collisionCenterX < obs.collisionCenterX && dx < attachWidth && dx >= attachWidth - xgap) {
+                    if (this.collisionCenterX < obs.collisionCenterX && dx <= attachWidth && dx >= attachWidth - xgap) {
                         console.log(`xgap:${xgap} dx:${dx} attachWidth:${attachWidth}`);
                         this.#rightBlock = true;
                         this.posX = obs.posX - this.spriteWidth;
@@ -273,7 +284,7 @@
                         isBlock = true;
                     }
 
-                    if (this.collisionCenterX > obs.collisionCenterX && dx < attachWidth && dx >= attachWidth - xgap) {
+                    if (this.collisionCenterX > obs.collisionCenterX && dx <= attachWidth && dx >= attachWidth - xgap) {
                         console.log(`xgap:${xgap} dx:${dx} attachWidth:${attachWidth}`);
                         this.#leftBlock = true;
                         this.posX = obs.posX + obs.spriteWidth;
@@ -283,19 +294,21 @@
                 }
 
                 if (dx < attachWidth) {
-                    if (this.collisionCenterY < obs.collisionCenterY && dy < attachHeight) {
+                    if (this.collisionCenterY < obs.collisionCenterY && dy <= attachHeight) {
                         this.#bottomBlock = true;
                         this.posY = obs.posY - this.spriteHeight;
                         dy = Math.abs(this.collisionCenterY - obs.collisionCenterY);
                         isBlock = true;
                         this.resetJumpStatus(false);
+                        this.jumpTimes = 0;
+                        this.velG = 0;
                         // console.log(`${this.#bottomBlock}`);
                         if (this.#SpaceKeyDown) {
                             this.resetJumpStatus(true);
                         }
                     }
 
-                    if (this.collisionCenterY > obs.collisionCenterY && dy < attachHeight) {
+                    if (this.collisionCenterY > obs.collisionCenterY && dy <= attachHeight) {
                         this.#topBlock = true;
                         this.resetJumpStatus(false);
                         this.posY = obs.posY + obs.spriteHeight;
@@ -341,7 +354,31 @@
         }
     }
 
-    function run() {
+    let averageFrames = 0;
+    let start, preTimeStamp, prestamp;
+    const fps = 60;
+    const msPerFrame = 1000 / fps; // 16.666666667
+
+    function run(timestamp) {
+        requestAnimationFrame(run);
+
+        if (start === undefined) {
+            start = timestamp;
+            preTimeStamp = prestamp = timestamp;
+        }
+        const elapsed = timestamp - preTimeStamp;
+        // const trueElapsed = timestamp - prestamp;
+        // prestamp = timestamp;
+        // console.log(trueElapsed);
+        
+        if (elapsed < msPerFrame) {
+            return
+        }
+        frames++;
+        const excessTime = elapsed % msPerFrame;
+        preTimeStamp = timestamp - excessTime;
+        // console.log(`timestamp: ${timestamp} elapsed: ${elapsed} excessTime: ${excessTime} pretimestamp: ${preTimeStamp} frames*msPerFrame: ${frames * msPerFrame}`);
+        
         // clear screen
         ctx.fillStyle = '#222';
         ctx.fillRect(- width / 2, - 3 * height / 4, width, height);
@@ -351,6 +388,12 @@
         ctx.lineWidth = 1;
         ctx.font = "36px arial";
         ctx.strokeText("Scroll Engine Test", - width / 2 + 10, - 3 * height / 4 + 36);
+
+        // fps
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = 1;
+        ctx.font = "18px sans-serif";
+        ctx.strokeText(`FPS: ${averageFrames.toFixed(2)}`, width / 2 - 100, - 3 * height / 4 + 36);
         
         obstacles.forEach(obs => {
             obs.draw();
@@ -359,6 +402,11 @@
         player.checkMovement();
         player.collisionDetect(obstacles);
         player.checkCollisionHappened();
-        player.checkBound();
-        requestAnimationFrame(run);
+        player.checkBound(); 
     }
+
+    setInterval(() => {
+        // console.log(frames);        
+        averageFrames = frames;
+        frames = 0;
+    }, 1000);
