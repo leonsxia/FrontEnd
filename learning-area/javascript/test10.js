f3f9c74b2594b0e1d8a301d77516e5f6d273c986
leonsxia <leon_s_xia@126.com>
2024-04-08 20:06:19 +0800

    update scroll game to node

---
+++ learning-area/javascript/test10.js
@@ -0,0 +1,23 @@
+function one () {
+    console.log("这是开始");
+
+    setTimeout(function cb() {
+        console.log("这是来自第一个回调的消息");
+    });
+
+    console.log("这是一条消息");
+
+    setTimeout(function cb1() {
+        console.log("这是来自第二个回调的消息");
+    }, 0);
+
+    console.log("这是结束");
+}
+
+function two() {
+    console.log('1');
+}
+
+one();
+two();
+console.log(3);
